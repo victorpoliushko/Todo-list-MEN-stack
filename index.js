a726e686e7410a12d6f2d947c7e7f45aba3aba83
victorpoliushko <milleniiumkb@gmail.com>
2020-08-10 16:34:23 +0300

    Fresh server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+async function start() {
+  try {
+    await mongoose.connect("", {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
