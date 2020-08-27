b1381c217f3828d9d2690905c7b07efbb5ff3263
victorpoliushko <milleniiumkb@gmail.com>
2020-08-27 08:06:56 +0300

    NavBar created

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const exphbs = require("express-handlebars");
+const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routes/todos');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const hbs = exphbs.create({
-  defaultLayout: "main",
-  extname: "hbs",
+  defaultLayout: 'main',
+  extname: 'hbs',
 });
 
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
-app.set("views", "views");
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+app.use(todoRoutes);
+
 async function start() {
   try {
-    await mongoose.connect("", {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-    });
+    await mongoose.connect(
+      'mongodb+srv://admin:admin@cluster0.ty6uc.mongodb.net/todos',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+      }
+    );
     app.listen(PORT, () => {
       console.log(`Server has been started on port ${PORT}`);
     });
