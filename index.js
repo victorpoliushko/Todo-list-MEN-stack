ac6581422786f24e4b33cad15b2876dac8aefa6e
victorpoliushko <milleniiumkb@gmail.com>
2020-10-23 20:20:56 +0300

    new styles

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const todoRoutes = require('./routes/todos');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,6 +16,7 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(todoRoutes);
 
 async function start() {
